7035abe5a7575c5ed81cdb9cb0b9c3460a3da33e
Hongqi Wang <hongqi.wang@alibaba-inc.com>
2017-09-26 19:22:32 -0700

    Mitigate function parameter issue

--- etl_job.go
+++ etl_job.go
@@ -1,16 +1,41 @@
 package sls
 
+import "encoding/json"
+
 type ETLJob struct {
-	JobName           string                 `json:"etlJobName"`
-	SourceConfig      *SourceConfig          `json:"sourceConfig"`
-	TriggerConfig     *TriggerConfig         `json:"triggerConfig"`
-	FunctionConfig    *FunctionConfig        `json:"functionConfig"`
-	FunctionParameter map[string]interface{} `json:"functionParameter"`
-	LogConfig         *JobLogConfig          `json:"logConfig"`
-	Enable            bool                   `json:"enable"`
+	JobName        string          `json:"etlJobName"`
+	SourceConfig   *SourceConfig   `json:"sourceConfig"`
+	TriggerConfig  *TriggerConfig  `json:"triggerConfig"`
+	FunctionConfig *FunctionConfig `json:"functionConfig"`
+
+	// TODO: change this to map[string]interface{} once log service fixes the format
+	FunctionParameter interface{}   `json:"functionParameter"`
+	LogConfig         *JobLogConfig `json:"logConfig"`
+	Enable            bool          `json:"enable"`
 
 	CreateTime int64 `json:"createTime"`
 	UpdateTime int64 `json:"updateTime"`
+}
+
+type etlJobAlias ETLJob
+
+// This can be removed once log service returns function parameter in json type.
+// "functionParameter":{"a":1} instead of "functionParameter":"{\"a\":1}"
+func (job *ETLJob) UnmarshalJSON(data []byte) error {
+	output := etlJobAlias{}
+	if err := json.Unmarshal(data, &output); err != nil {
+		return err
+	}
+	param := map[string]interface{}{}
+	paramStr, ok := output.FunctionParameter.(string)
+	if ok {
+		if err := json.Unmarshal([]byte(paramStr), &param); err != nil {
+			return err
+		}
+		output.FunctionParameter = param
+	}
+	*job = ETLJob(output)
+	return nil
 }
 
 type SourceConfig struct {
