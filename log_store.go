3fbd747d58cacc593279b02b43d55b6443b2874a
mengskysama <mengskysama@gmail.com>
2017-12-08 23:55:59 +0800

    fix memory leak

--- log_store.go
+++ log_store.go
@@ -40,6 +40,7 @@ func (s *LogStore) ListShards() (shardIDs []int, err error) {
 	if err != nil {
 		return nil, NewClientError(err.Error())
 	}
+	defer r.Body.Close()
 	buf, _ := ioutil.ReadAll(r.Body)
 	if r.StatusCode != http.StatusOK {
 		err := &Error{}
@@ -86,7 +87,7 @@ func (s *LogStore) PutLogs(lg *LogGroup) (err error) {
 	if err != nil {
 		return NewClientError(err.Error())
 	}
-
+	defer r.Body.Close()
 	body, _ = ioutil.ReadAll(r.Body)
 	if r.StatusCode != http.StatusOK {
 		err := new(Error)
@@ -107,9 +108,9 @@ func (s *LogStore) GetCursor(shardID int, from string) (cursor string, err error
 		s.Name, shardID, from)
 	r, err := request(s.project, "GET", uri, h, nil)
 	if err != nil {
-		return
+		return "", err
 	}
-
+	defer r.Body.Close()
 	buf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return
@@ -165,9 +166,9 @@ func (s *LogStore) GetLogsBytes(shardID int, cursor, endCursor string,
 
 	r, err := request(s.project, "GET", uri, h, nil)
 	if err != nil {
-		return
+		return nil, "", err
 	}
-
+	defer r.Body.Close()
 	buf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return
@@ -268,7 +269,7 @@ func (s *LogStore) GetHistograms(topic string, from int64, to int64, queryExp st
 	if err != nil {
 		return nil, NewClientError(err.Error())
 	}
-
+	defer r.Body.Close()
 	body, _ := ioutil.ReadAll(r.Body)
 	if r.StatusCode != http.StatusOK {
 		err := new(Error)
@@ -287,9 +288,9 @@ func (s *LogStore) GetHistograms(topic string, from int64, to int64, queryExp st
 		return nil, err
 	}
 	getHistogramsResponse := GetHistogramsResponse{
-		Progress: r.Header[ProgressHeader][0],
-		Count:    count,
-		Histograms:     histograms,
+		Progress:   r.Header[ProgressHeader][0],
+		Count:      count,
+		Histograms: histograms,
 	}
 
 	return &getHistogramsResponse, nil
@@ -310,7 +311,7 @@ func (s *LogStore) GetLogs(topic string, from int64, to int64, queryExp string,
 	if err != nil {
 		return nil, NewClientError(err.Error())
 	}
-
+	defer r.Body.Close()
 	body, _ := ioutil.ReadAll(r.Body)
 	if r.StatusCode != http.StatusOK {
 		err := new(Error)
@@ -352,7 +353,11 @@ func (s *LogStore) CreateIndex(index Index) error {
 	}
 
 	uri := fmt.Sprintf("/logstores/%s/index", s.Name)
-	_, err = request(s.project, "POST", uri, h, body)
+	r, err := request(s.project, "POST", uri, h, body)
+	if err != nil {
+		return err
+	}
+	r.Body.Close()
 	return err
 }
 
@@ -370,7 +375,10 @@ func (s *LogStore) UpdateIndex(index Index) error {
 	}
 
 	uri := fmt.Sprintf("/logstores/%s/index", s.Name)
-	_, err = request(s.project, "PUT", uri, h, body)
+	r, err := request(s.project, "PUT", uri, h, body)
+	if r != nil {
+		r.Body.Close()
+	}
 	return err
 }
 
@@ -396,7 +404,10 @@ func (s *LogStore) DeleteIndex() error {
 	}
 
 	uri := fmt.Sprintf("/logstores/%s/index", s.Name)
-	_, err = request(s.project, "DELETE", uri, h, body)
+	r, err := request(s.project, "DELETE", uri, h, body)
+	if r != nil {
+		r.Body.Close()
+	}
 	return err
 }
 
@@ -421,12 +432,13 @@ func (s *LogStore) GetIndex() (*Index, error) {
 	}
 
 	uri := fmt.Sprintf("/logstores/%s/index", s.Name)
-	resp, err := request(s.project, "GET", uri, h, body)
+	r, err := request(s.project, "GET", uri, h, body)
 	if err != nil {
+		return nil, err
 	}
-
 	index := &Index{}
-	data, _ := ioutil.ReadAll(resp.Body)
+	defer r.Body.Close()
+	data, _ := ioutil.ReadAll(r.Body)
 	err = json.Unmarshal(data, index)
 	if err != nil {
 		return nil, err
