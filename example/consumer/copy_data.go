773264c904bf528bff63637b988d2c779891c2c4
xuxiaohang <evan@xuxiaohangdeMacBook-Air.local>
2019-02-27 11:19:00 +0800

    add example explain

--- example/consumer/copy_data.go
+++ example/consumer/copy_data.go
@@ -6,19 +6,24 @@ import (
 	"github.com/aliyun/aliyun-log-go-sdk/consumer"
 )
 
+// README :
+// This is an E2E test, which creates another logstore under the same project to simulate consumption.
+// If you don't want to use this method, you can comment out 37-45 lines of code and override your own prcess function
+// Be careful not to change the parameter type of process function.
+
 var option consumerLibrary.LogHubConfig
 var client sls.Client
 var logStore *sls.LogStore
 
 func main() {
 	option = consumerLibrary.LogHubConfig{
-		Endpoint:           "",
-		AccessKeyID:        "",
-		AccessKeySecret:    "",
-		Project:            "",
-		Logstore:           "",
+		Endpoint:          "",
+		AccessKeyID:       "",
+		AccessKeySecret:   "",
+		Project:           "",
+		Logstore:          "",
 		ConsumerGroupName: "",
-		ConsumerName:       "",
+		ConsumerName:      "",
 		// This options is used for initialization, will be ignored once consumer group is created and each shard has been started to be consumed.
 		// Could be "begin", "end", "specific time format in time stamp", it's log receiving time.
 		CursorPosition: consumerLibrary.BEGIN_CURSOR,
