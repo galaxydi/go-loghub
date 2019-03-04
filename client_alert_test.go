e5e47562fb4a9cfbbdda6f872a1fa8dca31a8ae0
liketic <ke.like@outlook.com>
2019-03-04 11:53:25 +0800

    Fix broken alert api related tests

--- client_alert_test.go
+++ client_alert_test.go
@@ -1,34 +1,82 @@
 package sls
 
 import (
+	"fmt"
+	"github.com/golang/glog"
+	"github.com/stretchr/testify/suite"
+	"os"
 	"testing"
 	"time"
 )
 
-var testProject = "test-project-name"
-var testAlertName = "alert-test-name"
-var testDashboard = "test-dashboard-name"
+func TestAlert(t *testing.T) {
+	suite.Run(t, new(AlertTestSuite))
+	glog.Flush()
+}
 
-func client() *Client {
-	return &Client{
-		AccessKeyID:     "",
-		AccessKeySecret: "",
-		Endpoint:        "",
+type AlertTestSuite struct {
+	suite.Suite
+	endpoint        string
+	projectName     string
+	logstoreName    string
+	logShipperRole  string
+	accessKeyID     string
+	accessKeySecret string
+	Project         *LogProject
+	Logstore        *LogStore
+	alertName       string
+	dashboardName   string
+	client          *Client
+}
+
+func (s *AlertTestSuite) SetupTest() {
+	s.endpoint = os.Getenv("LOG_TEST_ENDPOINT")
+	s.projectName = os.Getenv("LOG_TEST_PROJECT")
+	s.logstoreName = os.Getenv("LOG_TEST_LOGSTORE")
+	s.accessKeyID = os.Getenv("LOG_TEST_ACCESS_KEY_ID")
+	s.accessKeySecret = os.Getenv("LOG_TEST_ACCESS_KEY_SECRET")
+	slsProject, err := NewLogProject(s.projectName, s.endpoint, s.accessKeyID, s.accessKeySecret)
+	s.Nil(err)
+	s.NotNil(slsProject)
+	s.Project = slsProject
+	s.dashboardName = fmt.Sprintf("go-test-dashboard-%d", time.Now().Unix())
+	s.alertName = fmt.Sprintf("go-test-alert-%d", time.Now().Unix())
+	s.client = &Client{
+		AccessKeyID:     s.accessKeyID,
+		AccessKeySecret: s.accessKeySecret,
+		Endpoint:        s.endpoint,
 	}
 }
 
-func TestClient_CreateAlert(t *testing.T) {
-	client().DeleteAlert(testProject, testAlertName)
-	err := createAlert()
+func (s *AlertTestSuite) TestClient_CreateAlert() {
+	err := s.createAlert()
+	s.Require().Nil(err)
+	err = s.client.DeleteAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+}
+
+func (s *AlertTestSuite) createAlert() error {
+	alerts, _, _, err := s.client.ListAlert(s.projectName, "", "", 0, 100)
+	s.Require().Nil(err)
+	for _, x := range alerts {
+		err = s.client.DeleteAlert(s.projectName, x.Name)
+		s.Require().Nil(err)
+	}
+	dashboard := Dashboard{
+		DashboardName: s.dashboardName,
+		DisplayName:   "test-dashboard",
+		Description:   "test dashboard",
+		ChartList:     []Chart{},
+	}
+	err = s.client.CreateDashboard(s.projectName, dashboard)
 	if err != nil {
-		t.Fatal(err)
+		slsErr := err.(*Error)
+		if slsErr.Message != "specified dashboard already exists" {
+			s.Require().Fail(slsErr.Message)
+		}
 	}
-	client().DeleteAlert(testProject, testAlertName)
-}
-
-func createAlert() error {
 	alert := &Alert{
-		Name:        testAlertName,
+		Name:        s.alertName,
 		State:       "Enabled",
 		DisplayName: "AlertTest",
 		Description: "Description for alert",
@@ -47,7 +95,7 @@ func createAlert() error {
 					LogStore:     "test-logstore",
 				},
 			},
-			Dashboard:  testDashboard,
+			Dashboard:  s.dashboardName,
 			MuteUntil:  time.Now().Unix() + 3600,
 			Throttling: "5m",
 			Condition:  "count > 0",
@@ -66,103 +114,75 @@ func createAlert() error {
 			NotifyThreshold: 1,
 		},
 	}
-	return client().CreateAlert(testProject, alert)
+	return s.client.CreateAlert(s.projectName, alert)
 }
 
-func TestClient_UpdateAlert(t *testing.T) {
-	createAlert()
-	alert, err := client().GetAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
+func (s *AlertTestSuite) TestClient_UpdateAlert() {
+	err := s.createAlert()
+	s.Require().Nil(err)
+	alert, err := s.client.GetAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
 	alert.DisplayName = "new display name"
 	alert.CreateTime = 0
 	alert.LastModifiedTime = 0
-	err = client().UpdateAlert(testProject, alert)
-	if err != nil {
-		t.Fatal(err)
-	}
-	alert, err = client().GetAlert(testProject, testAlertName)
-	if alert.DisplayName != "new display name" {
-		t.Fatal("update alert failed")
-	}
-	client().DeleteAlert(testProject, testAlertName)
+	err = s.client.UpdateAlert(s.projectName, alert)
+	s.Require().Nil(err)
+	alert, err = s.client.GetAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	s.Require().Equal("new display name", alert.DisplayName, "update alert failed")
+	err = s.client.DeleteAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
 }
 
-func TestClient_DeleteAlert(t *testing.T) {
-	createAlert()
-	_, err := client().GetAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = client().DeleteAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = client().GetAlert(testProject, testAlertName)
-	if err == nil {
-		t.Fatal(err)
-	}
+func (s *AlertTestSuite) TestClient_DeleteAlert() {
+	err := s.createAlert()
+	s.Require().Nil(err)
+	_, err = s.client.GetAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	err = s.client.DeleteAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	_, err = s.client.GetAlert(s.projectName, s.alertName)
+	s.Require().NotNil(err)
 }
 
-func TestClient_DisableAndEnableAlert(t *testing.T) {
-	createAlert()
-	err := client().DisableAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	alert, err := client().GetAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if alert.State != "Disabled" {
-		t.Fatal("disable alert failed")
-	}
-	err = client().EnableAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	alert, err = client().GetAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if alert.State != "Enabled" {
-		t.Fatal("enable alert failed")
-	}
-	client().DeleteAlert(testProject, testAlertName)
+func (s *AlertTestSuite) TestClient_DisableAndEnableAlert() {
+	err := s.createAlert()
+	s.Require().Nil(err)
+	err = s.client.DisableAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	alert, err := s.client.GetAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	s.Require().Equal("Disabled", alert.State, "disable alert failed")
+	err = s.client.EnableAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	alert, err = s.client.GetAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	s.Require().Equal("Enabled", alert.State, "enable alert failed")
+	err = s.client.DeleteAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
 }
 
-func TestClient_GetAlert(t *testing.T) {
-	createAlert()
-	getAlert, err := client().GetAlert(testProject, testAlertName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if getAlert.Name != "alert-test-name" {
-		t.Fail()
-	}
+func (s *AlertTestSuite) TestClient_GetAlert() {
+	err := s.createAlert()
+	s.Require().Nil(err)
+	getAlert, err := s.client.GetAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
+	s.Require().Equal(getAlert.Name, s.alertName)
+	err = s.client.DeleteAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
 }
 
-func TestClient_ListAlert(t *testing.T) {
-	err := createAlert()
-	if err != nil {
-		t.Fatal(err)
-	}
-	alerts, total, count, err := client().ListAlert(testProject, "", "", 0, 100)
-	if err != nil {
-		t.Fatal(err)
-	}
+func (s *AlertTestSuite) TestClient_ListAlert() {
+	err := s.createAlert()
+	s.Require().Nil(err)
+	alerts, total, count, err := s.client.ListAlert(s.projectName, "", "", 0, 100)
+	s.Require().Nil(err)
 	if total != 1 || count != 1 {
-		t.Log(total)
-		t.Log(count)
-		t.Fatal("list alert failed")
+		s.Require().Fail("list alert failed")
 	}
-	if len(alerts) != 1 {
-		t.Fatal("there should be only one alert")
-	}
+	s.Require().Equal(1, len(alerts), "there should be only one alert")
 	alert := alerts[0]
-	if alert.Name != testAlertName {
-		t.Fatal("list alert failed")
-	}
-	client().DeleteAlert(testProject, testAlertName)
+	s.Require().Equal(s.alertName, alert.Name, "list alert failed")
+	err = s.client.DeleteAlert(s.projectName, s.alertName)
+	s.Require().Nil(err)
 }
