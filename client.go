563d55db5e1aac6243d277a348ed50d2be222cc7
铭毅 <chengsheng.xcs@alibaba-inc.com>
2022-04-22 15:16:00 +0800

    fix dead lock since golang doesn't support ReentrantLock
    Signed-off-by: 铭毅 <chengsheng.xcs@alibaba-inc.com>

--- client.go
+++ client.go
@@ -95,6 +95,10 @@ type Client struct {
 func convert(c *Client, projName string) *LogProject {
 	c.accessKeyLock.RLock()
 	defer c.accessKeyLock.RUnlock()
+	return convertLogProject(c, projName)
+}
+
+func convertLogProject(c *Client, projName string) *LogProject {
 	p, _ := NewLogProject(projName, c.Endpoint, c.AccessKeyID, c.AccessKeySecret)
 	p.SecurityToken = c.SecurityToken
 	p.UserAgent = c.UserAgent
--- client_store.go
+++ client_store.go
@@ -16,7 +16,7 @@ import (
 
 func convertLogstore(c *Client, project, logstore string) *LogStore {
 	c.accessKeyLock.RLock()
-	proj := convert(c, project)
+	proj := convertLogProject(c, project)
 	c.accessKeyLock.RUnlock()
 	return &LogStore{
 		project: proj,
